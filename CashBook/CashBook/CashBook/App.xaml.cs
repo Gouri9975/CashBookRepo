8e4182230e93cb3c6e3dee667e2f4f1075b868af
Gouri Nikam <SachinN@odata.com>
2022-01-22 12:28:19 +0530

    added cashbook solution project this is my first commit

---
+++ CashBook/CashBook/CashBook/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using CashBook.Services;
+using CashBook.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CashBook
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ CashBook/CashBook/CashBook/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CashBook.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CashBook.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
